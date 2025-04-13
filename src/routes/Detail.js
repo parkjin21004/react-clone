62f6237d39fb45f8ceb3c6930112104df1e05151
parkjin21004 <parkjin21004@gmail.com>
2025-04-14 01:29:22 +0900

    Fix(Detail): getMovie 함수를 useEffect 내에 선언

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,13 +4,13 @@ import { useParams } from "react-router-dom";
 function Detail() {
   const { id } = useParams();
   const [movie, setMovie] = useState();
-  const getMovie = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    setMovie(json.data.movie);
-  };
   useEffect(() => {
+    const getMovie = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      setMovie(json.data.movie);
+    };
     getMovie();
   }, []);
 
